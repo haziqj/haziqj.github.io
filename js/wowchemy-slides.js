8f6a5fe08704cbe9bd15ca3f85275ec02376f960
haziqj <haziqj@users.noreply.github.com>
2021-09-11 18:03:41 +0000

    deploy: haziqj/hj-site@76a570f7d81f6729acbaa88a39380001d74899ff

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = null;
+  var slides = { highlight_style: "dracula", theme: "white" };
 
   // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5@v5.0.0-20210629192904-559885af86b7/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
